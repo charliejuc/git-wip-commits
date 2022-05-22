e0004be9408e918a9758453bc2f10a8d53afc426
Juan Carlos <charliejuc@gmail.com>
2022-05-22 16:23:47 +0200

    WIP Adds levelUpTraining

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { pikachu, totodile } = require("./pokemons");
 
 const levelAttributesMultipliers = {
-  health: 10,
+  health: 20,
   attack: 5,
   defense: 5,
 };
@@ -30,7 +30,37 @@ const isValidPokemon = (pokemon) => {
 };
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-const levelUpTraining = (pokemon) => async (levels) => {};
+const levelUpTraining = (pokemon) => {
+  const _levelUpTraining = async (
+    levels,
+    _pokemonLeveledUp,
+    _remainingLevels
+  ) => {
+    const remainingLevels = Number.isInteger(_remainingLevels)
+      ? _remainingLevels
+      : levels;
+    const pokemonToLevelUp = _pokemonLeveledUp || pokemon;
+
+    if (remainingLevels <= 0) {
+      return _pokemonLeveledUp;
+    }
+
+    const msToLevelUp = 300 * pokemon.level;
+    await sleep(msToLevelUp);
+
+    const newLevel = pokemonToLevelUp.level + 1;
+    const pokemonLeveledUp = {
+      ...pokemonToLevelUp,
+      level: newLevel,
+      attack: pokemonToLevelUp.attack + newLevel,
+      defense: pokemonToLevelUp.defense + newLevel,
+    };
+
+    return levelUpTraining(levels, pokemonLeveledUp, remainingLevels - 1);
+  };
+
+  return _levelUpTraining;
+};
 
 console.log(`Valid ${pikachu.name}:`, isValidPokemon(pikachu));
 console.log(`Valid ${totodile.name}:`, isValidPokemon(totodile));
